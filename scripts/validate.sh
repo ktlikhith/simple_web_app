92f6f95c15d505bb95fa8ef9e6ff5d8d86a9f49c
K T Likhith <ktlikhith@Ks-MacBook-Air.local>
2024-09-06 15:45:12 +0530

    updated jenkins file

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,10 +1,13 @@
+# #!/bin/bash
+# sleep 10s
+# status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:18000)
+# if [ "$status_code" -eq 200 ] ; then
+#   echo "Service is working fine"
+#   exit 0
+# else
+#   echo "Site status changed to $status_code"
+#   exit 1
+# fi
+
 #!/bin/bash
-sleep 10s
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:18000)
-if [ "$status_code" -eq 200 ] ; then
-  echo "Service is working fine"
-  exit 0
-else
-  echo "Site status changed to $status_code"
-  exit 1
-fi
+curl -s http://localhost:3000/health | grep "OK"
