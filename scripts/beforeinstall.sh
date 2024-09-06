017ec85d614874701c18845476cafbf5c60a70a5
K T Likhith <ktlikhith@Ks-MacBook-Air.local>
2024-09-06 15:41:34 +0530

    updated jenkins file

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -4,5 +4,7 @@ if [ ! -d /home/ubuntu/dist ]; then
     exit 0
 else
     cd /home/ubuntu/dist
+    sudo pm2 stop api
+    sudo pm2 delete api
     cd /home/ubuntu && sudo rm -rf dist
 fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/dist
 sudo npm install
-sudo npm start src/app.js 
+sudo npm install -g pm2
+sudo pm2 start src/app.js -f --name api
